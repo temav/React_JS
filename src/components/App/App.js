9300ed2534c8b5058d5efe57e908b88da957ae84
Artem <43587572+temav@users.noreply.github.com>
2020-04-02 18:38:09 +0300

    create

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,25 @@
 import React, { PureComponent } from 'react';
-import { Switch, withRouter } from 'react-router-dom';
+import { Switch, withRouter, Link, Route, Redirect } from 'react-router-dom';
 import { AuthorizeProvider } from 'components/AuthorizeProvider';
-
+import Login from '../Login';
+import Public from '../Public';
+import './App.css';
 export class App extends PureComponent {
   render() {
     return (
       <AuthorizeProvider>
         <div>
-          <Switch />
+            <nav>
+            <Link to="/">Главная</Link>
+            <Link to="/login">Войти</Link>
+            <Link to="/private">Секретная страница</Link>
+            </nav>
         </div>
+        <Switch>
+          <Route exact path="/" component={Public} />
+          <Route path="/login" component={Login} />
+          <Redirect from="/private" to="/login" />    
+        </Switch>
       </AuthorizeProvider>
     );
   }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,14 +3,32 @@ import { AuthHOC } from 'components/AuthorizeProvider';
 import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    error: false
+  };
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
+  // handleLog = () => {
+  //   const {authorizeUser} = this.props;
+  //   authorizeUser(...this.state);
+  // };
 
   render() {
-    const { isAuthorized } = this.props;
-
-    return isAuthorized ? <Redirect to="/" /> : null;
+    const { isAuthorized,  authorizeUser} = this.props;
+    const data = this.state;
+    return isAuthorized ? <Redirect to="/" /> : 
+    <div>
+      <input name="email" onChange={this.handleChange}/>
+      <input name="password" onChange={this.handleChange}/>
+      <button onClick={()=>{ console.log(authorizeUser(data.email, data.password));
+        this.setState({error: !authorizeUser(data.email, data.password)});
+      }
+      }>Log in</button>
+      { this.state.error === false ? undefined : <p>error</p>}
+    </div>;
   }
 }
 
